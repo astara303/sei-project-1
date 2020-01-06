2d4dc6a202bbfa6f07306d97aea772792ada53ac
Astara Cambata <AstaraCambata@gmail.com>
2020-01-06 10:37:09 +0000

    major changes to logic. Removed firing button

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -5,41 +5,65 @@ function init() {
 
   STAGE ONE: SET-UP
 
-  Randomly place different sized ships on player and computer grid
+  Have player choose the location of their ships by clicking grid cells
+  Randomly generate the computer's ship placement but do not display it on the board
+  Save these chosen grid cells in the ship location array
+  -make sure clicks do not change ship placement one the set-up phase is complete
   -the ship cannot go off the board
   -the ship cannot intersect another ship
-  -the computer's grid is shown to record the player's missile-fires in stage two, but the ships are not displayed
   
 
   STAGE TWO: PLAYER MISSILE FIRE
 
-  Player input is taken, such as A7 or B1
-  This input in converted into a grid cell number in the grid array
-  A missile can MISS, HIT, and SINK a ship. Player's turn is displayed on computer grid
+  Player clicks a grid cell to fire a missile
+  A missile can MISS, HIT, and SINK a ship
+  Player's turn is displayed on computer grid
 
 
-  STAGE THREE: COMPUTER MISSILE FIRE
+  STAGE THREE: COMPUTER MISSILE FIRE (can make the game 2 player for MVP)
 
   The computer choice is generated once per turn, after the player has had their turn
-  Randomised cell is chosen, such as 11 or 68
-  A missile can MISS, HIT, and SINK a ship. Computer's turn is played on player grid (their hits and misses are shown on player board)
+  Randomised cell is chosen to fire missile
+  A missile can MISS, HIT, and SINK a ship
+  Computer's turn is displayed on player grid
+
+  STAGE FOUR: DECLARE A WINNER
+
+  Determine when all of a player's ships have been sunk
 
 
   **GAME LOGIC**
 
+  SET-UP.) Visibly place the ships on the player grid
 
-  SET-UP.) Create each ship's position and length on the grid
+  Tell the player they are setting a ship with the length of 2 (blue)
+  After placement, save these grid cells in ship1 location array
+  When 2 values are given, present the next instructions
+
+  Tell the player they are setting a ship with the length of 3 (yellow)
+  After placement, save these grid cells in ship2 location array
+  When 3 values are given, present the next instructions
+
+  Tell the player they are setting a ship with the length of 4 (green)
+  After placement, save these grid cells in ship3 location array
+  When 4 values are given, present the next instructions
+
+  Tell the player they are setting a ship with the length of 5 (orange)
+  After placement, save these grid cells in ship1 location array
+  When 5 values are given, tell the player to click the "initiate game" button to start firing
+
+  Player can press the "set-up" button again to clear and restart placement
+
+
+  SET-UP.) Place ships on the computer grid (hidden)
+
+  When player clicks a button, all computer ships are placed but not shown
 
   Randomise a number 0-99 (This corresponds to an index in the array of grid cells)
   Use that random number (1, 22 etc) to add 1, 2, or more grid cells on to it to create horizonal or vertical length 
   (add grid cell 2 to 3 to create ship 1 horizontally) 
   (add grid cells C1 and D1 to B1 to create ship 2 vertically)
-
-
-  SET-UP.) Visibly place the ships on the grid
-
-  Create a function that uses the cells stored in ship length array to add a class to each cell
-  Ship 1 can be blue, ship 2 can be green, etc.
+  Push these values into the computer ship location array
   
 
   SET-UP.) How will the ships not intersect?
@@ -52,35 +76,45 @@ function init() {
   I should look to the pikachu grid in order to use this method to restrict the borders of the grid
 
 
-  PLAYER MISSILE-FIRE.) Convert player input (A1), into its corresponding grid cell index ('A1' === gridCells[1])?
+  PLAYER MISSILE-FIRE.) Player chosen missile-fire
   
-  Store the player input in a variable
-  I could check this variable against a list that has each cell labelled as A1 = index 1, E8 = index 84 etc.
+  Player clicks a grid cell
+  A click event reads if this grid cell on player grid has a hit, miss, or sink on the computer grid
 
 
   PLAYER MISSILE-FIRE.) Display grid cell as MISS or HIT
   
-  a function will convert the player's missile-fire input into a grid cell index
-  a second function will check if this index corresponds to any computerShipLocation array grid cells
+  a function will check if the chosen cell index corresponds to any computer ship location array grid cells
   if it does, a class of "HIT" will be added to the grid cell and displayed on the computer grid
   if it doesn't, a class of "MISS" will be added to the grid cell and displayed on the computer grid
-  Create a class that adds a color to the grid to display on a missed or hit grid cell. Have a key on the page that shows that yellow = miss, red = hit etc.
-  or Save an asset that is an image that says "MISS" or "HIT" and it fit into each grid cell when required
-
-
-  PLAYER MISSILE-FIRE.) Display a ship as SUNK after all ship grid cells have been HIT
   
+  Create a class that adds a color to the grid to display on a missed or hit grid cell. Have a key on the page that shows that yellow = miss, red = hit etc.
+  or 
+  Save an asset that is an image that says "MISS" or "HIT" and it fit into each grid cell when required
+
+
+  PLAYER MISSILE-FIRE.) Display a computer ship's cells as SUNK after all grid cells have been HIT
+   
   Player's missile-fire input is converted into a grid cell index and a class of "HIT" has been added
-  A function checks if a class of "hit" has been added to all grid cells in a computerShipLocation's array
+  A function checks if a class of "hit" has been added to all grid cells in a computer ship location's array
   If all grid cells have been hit, change isSunk from false to true
   if isSunk = true, add a class of SUNK to the ship grid cells
+  
   Create a class that adds a color to the grid to display on all sunk grid cells. Have a key on the page that shows that black = sunk
-  or Save an asset that is an image that says "SUNK" and it fit into each grid cell when required
+  or 
+  Save an asset that is an image that says "SUNK" and it fit into each grid cell when required
 
 
-  COMPUTER MISSLE-FIRE.)
-  Randomly generate a grid cell to target on the player's board
-  Read if the
+  COMPUTER MISSILE-FIRE.) Computer generated missile fire
+  Randomly generate a grid cell (0-99) to target on the player's board
+  Read if the cell is contained in a player ship location array
+
+  COMPUTER MISSILE-FIRE.) Intelligent missile-firing
+  If a turn is a HIT, the computer should fire at cells surrounding the HIT cell
+  Should be able to use similar logic to how I auto-generated ships on the grid
+
+  COMPUTER MISSILE-FIRE.) Display player grid cells as HIT, MISS, or SUNK
+  
   */
 
 
@@ -158,7 +192,6 @@ function init() {
   })
 
   //COMPUTER GRID
-  //Will be shown next to player's grid 
   //ships will not be shown, but MISS, and HIT, and SUNK will.
   Array(width * width).join('.').split('.').forEach(() => {
     const cell = document.createElement('div')
@@ -183,17 +216,16 @@ function init() {
   //so it must be able to determine that grid cell 13 is surrounded by 12 and 14 (current chosen cell number +1 (++) or -1 (--)) and 3 and 23 (current chosen cell number +10 or -10)
   
 
+
   //STAGE TWO: PLAYER MISSILE FIRE
 
-  //a function that retreives the player data and turns it into an index of the computer grid
-
-  //an event that, when the submit button is clicked, the playerData is console logged
-  fireButton.addEventListener('click', e => {
-    e.preventDefault()
-    //this is logging the number of times its clicked, not logging the value of user input I saved as playerData
-    console.log('player cell choice was', playerData.value)
-    //this logs undefined
-    //console.log(playerData.value)
-  })
+  // //an event that, when the submit button is clicked, the playerData is console logged
+  // fireButton.addEventListener('click', e => {
+  //   e.preventDefault()
+  //   //this is logging the number of times its clicked, not logging the value of user input I saved as playerData
+  //   console.log('player cell choice was', playerData.value)
+  //   //this logs undefined
+  //   //console.log(playerData.value)
+  // })
 }
 window.addEventListener('DOMContentLoaded', init)
